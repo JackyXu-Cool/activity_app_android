34592ab1b83119868cae96fe67b5dfe33c568a04
jxu477 <jxu477@gatech.edu>
2020-10-01 19:33:13 -0400

    first commit

---
+++ app/src/main/java/activity/app/android/MainActivity.java
@@ -0,0 +1,15 @@
+package activity.app.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Replace "R.layout.activity_appy_page" with the xml you want to present
+        // setContentView(R.layout.activity_apply_page);
+    }
+}
