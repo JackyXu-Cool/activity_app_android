5d34c046b4499fa165bc6df5bf0ac420a8559a5d
jxu477 <jxu477@gatech.edu>
2020-10-14 12:00:24 -0400

    check loading is finished

--- app/src/main/java/activity/app/android/ApplyGroupActivity.java
+++ app/src/main/java/activity/app/android/ApplyGroupActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -38,10 +40,15 @@ public class ApplyGroupActivity extends AppCompatActivity{
     public native String getNativeKey1();
     String key1 = new String(Base64.decode(getNativeKey1(), Base64.DEFAULT));
 
+    // Declare useful elemnt from the page
     Button displayDocs;
     Button displayHighlights;
     Button displayMoments;
+    ImageView avatar;
     ViewFlipper slidingContentSelector;
+    LinearLayout progressView;
+    LinearLayout groupContent;
+    GroupApplyPageBinding mbinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +58,10 @@ public class ApplyGroupActivity extends AppCompatActivity{
         Bmob.initialize(this, key1);
 
         // Apply data binding
-        GroupApplyPageBinding mbinding = DataBindingUtil.setContentView(this, R.layout.group_apply_page);
+        mbinding = DataBindingUtil.setContentView(this, R.layout.group_apply_page);
 
-        // Fetch group from database and then do basic setup
-        // TODO: How group will be passed in??
-        // TODO: Loading takes some time (how to make sure the page is fully loaded and then present it to the users?)
-        try {
-            BmobQuery<Group> bmobQuery = new BmobQuery<Group>();
-            bmobQuery.getObject("90eddaede6", new QueryListener<Group>() {
-                @Override
-                public void done(Group group,BmobException e) {
-                    if(e==null){
-                        mbinding.setGroup(group);
-
-                        // Set Image view, group introduction, and created date
-                        setGroupCover(group.getCoverURL());
-                        setGroupIntro(group.getGroupIntroduction());
-                        setGroupCreatedDate(group.getCreatedDate());
-
-                    }else{
-                        throw new NoSuchElementException("Cannot find group");
-                    }
-                }
-            });
-        } catch(Exception e) {
-            // TODO: Reject to render anything and retain in the last page
-        }
+        groupContent = findViewById(R.id.group);
+        progressView = findViewById(R.id.progressbar_view);
 
         // Set up toolbars
         Toolbar toolbar = findViewById(R.id.my_toolbar);
@@ -85,17 +70,91 @@ public class ApplyGroupActivity extends AppCompatActivity{
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.returnbutton);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Define the buttons on sliding panel
+        // Instantiate the buttons on sliding panel
         displayDocs = findViewById(R.id.activityDocsButton);
         displayHighlights = findViewById(R.id.activityHighlightsButton);
         displayMoments = findViewById(R.id.activityMomentsButton);
         slidingContentSelector = findViewById(R.id.slidingDisplaySelector);
+        avatar = findViewById(R.id.avatar);
 
-
-        // Set up sliding button groups
+        // Set up sliding button groups functionality
         sliding_buttongroups_setup();
         slidingContentSelector.setDisplayedChild(1);
 
+        new Task().execute();
+    }
+
+    class Task extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            progressView.setVisibility(View.VISIBLE);
+            groupContent.setVisibility(View.GONE);
+            avatar.setVisibility(View.GONE);
+
+            // Fetch group from database and then do basic setup
+            // TODO: How will groupid be passed in??
+            try {
+                BmobQuery<Group> bmobQuery = new BmobQuery<Group>();
+                bmobQuery.getObject("90eddaede6", new QueryListener<Group>() {
+                    @Override
+                    public void done(Group group,BmobException e) {
+                        if(e==null){
+                            mbinding.setGroup(group);
+
+                            // Set Image view, group introduction, and created date
+                            setGroupCover(group.getCoverURL());
+                            setGroupIntro(group.getGroupIntroduction());
+                            setGroupCreatedDate(group.getCreatedDate());
+
+                        }else{
+                            throw new NoSuchElementException("Cannot find group");
+                        }
+                    }
+                });
+            } catch(Exception e) {
+                // TODO: Reject to render anything and retain in the last page
+            }
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            progressView.setVisibility(View.GONE);
+            groupContent.setVisibility(View.VISIBLE);
+            avatar.setVisibility(View.VISIBLE);
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            while (avatar.getDrawable() == null) {
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        // Private method that set the created date of this group
+        private void setGroupCreatedDate(Date date) {
+            DateFormat df = new SimpleDateFormat("yyyy年MM月dd日", Locale.CHINA);
+            TextView txtCreatedDate = findViewById(R.id.groupDateText);
+            txtCreatedDate.setText("成立时间：" + df.format(date));
+        }
+
+        // Private method that set up group's cover
+        private void setGroupCover(String url){
+            Glide.with(ApplyGroupActivity.this).load(url).
+                    apply(new RequestOptions().override(600, 400)).centerCrop().into(avatar);
+        }
+
+        // Private method that set up group's introduction
+        private void setGroupIntro(String intro){
+            TextView txtGroupIntro = findViewById(R.id.group_intro_content);
+            txtGroupIntro.setText(intro);
+        }
     }
 
     @Override
@@ -142,25 +201,5 @@ public class ApplyGroupActivity extends AppCompatActivity{
             displayHighlights.setSelected(false);
             displayMoments.setSelected(true);
         }
-    }
-
-    // Private method that set the created date of this group
-    private void setGroupCreatedDate(Date date) {
-        DateFormat df = new SimpleDateFormat("yyyy年MM月dd日", Locale.CHINA);
-        TextView txtCreatedDate = findViewById(R.id.groupDateText);
-        txtCreatedDate.setText("成立时间：" + df.format(date));
-    }
-
-    // Private method that set up group's cover
-    private void setGroupCover(String url){
-        ImageView avatar = findViewById(R.id.avatar);
-        Glide.with(ApplyGroupActivity.this).load(url).
-                apply(new RequestOptions().override(600, 400)).centerCrop().into(avatar);
-    }
-
-    // Private method that set up group's introduction
-    private void setGroupIntro(String intro){
-        TextView txtGroupIntro = findViewById(R.id.group_intro_content);
-        txtGroupIntro.setText(intro);
     }
 }
